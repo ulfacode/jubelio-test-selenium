5762cbfdbefcb27bb66e5d62f6ed349365bd8ee8
Ulfatun Nasikhah <ulfatunnasikhah49@gmail.com>
2024-12-30 21:49:18 +0700

    add case login

--- test/login.js
+++ test/login.js
@@ -1,18 +1,64 @@
 const { Builder, By, Browser } = require("selenium-webdriver");
-const { Driver } = require("selenium-webdriver/chrome");
+// const { Driver } = require("selenium-webdriver/chrome");
 
 
 async function Login_Positif() {
-    driver = await new Builder().forBrowser(Browser.CHROME).build();
+    let driver = await new Builder().forBrowser(Browser.CHROME).build();
     await driver.get('https://app2.jubelio.com/login');
 
-    let textBox = await Driver.findElement(By.name('email'));
-    let textPassword = await Driver.findElement(By.name('password'));
+    let textBox = await driver.findElement(By.name('email'));
+    let textPassword = await driver.findElement(By.name('password'));
 
-    await textBox. seynKeys('Selenium');
-    await textPassword. seynKeys('12345678a');
+    await textBox.sendKeys('test.Ulfa1@yopmail.com');
+    await textPassword.sendKeys('12345678Aa!');
 
-    let sumbitButton = await Driver.findElement(By.css('button'));
+    let sumbitButton = await driver.findElement(By.css('button'));
     await sumbitButton.click();
 }
+
+async function Login_Negative1() {
+    let driver = await new Builder().forBrowser(Browser.CHROME).build();
+    await driver.get('https://app2.jubelio.com/login');
+    
+    let textBox = await driver.findElement(By.name('email'));
+    let textPassword = await driver.findElement(By.name('password'));
+    
+    await textBox.sendKeys('test.ulfa1');  //email tidak valid
+    await textPassword.sendKeys('12345678Aa!');
+    
+    let sumbitButton = await driver.findElement(By.css('button'));
+    await sumbitButton.click();
+}
+
+async function Login_Negative2() { //email tidak terdaftar
+    let driver = await new Builder().forBrowser(Browser.CHROME).build();
+    await driver.get('https://app2.jubelio.com/login');
+    
+    let textBox = await driver.findElement(By.name('email'));
+    let textPassword = await driver.findElement(By.name('password'));
+    
+    await textBox.sendKeys('test.ulfa2@yopmail.com');
+    await textPassword.sendKeys('12345678Aa!');
+    
+    let sumbitButton = await driver.findElement(By.css('button'));
+    await sumbitButton.click();
+}
+
+async function Login_Negative3() { //pass tidak sesuai
+    let driver = await new Builder().forBrowser(Browser.CHROME).build();
+    await driver.get('https://app2.jubelio.com/login');
+    
+    let textBox = await driver.findElement(By.name('email'));
+    let textPassword = await driver.findElement(By.name('password'));
+    
+    await textBox.sendKeys('test.ulfa1@yopmail.com');
+    await textPassword.sendKeys('12345678Aa');
+    
+    let sumbitButton = await driver.findElement(By.css('button'));
+    await sumbitButton.click();
+}
+
 Login_Positif();
+Login_Negative1();
+Login_Negative2();
+Login_Negative3();
